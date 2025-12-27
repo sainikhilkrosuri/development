d99977e4358b36202c74cabade8b719cd4806063
sainikhilkrosuri <sainikhilkrosuri@gmail.com>
2025-12-27 02:29:07 -0600

    Updated new models to store webhook key

--- webhook/models.py
+++ webhook/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class newWebhhook(models.Model):
+    user_id = models.CharField(max_length=100)
+    webhook_id = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.user_id
