fd5e7ee945407bd1c63568fcbea18eb6bd46eb39
sainikhilkrosuri <sainikhilkrosuri@gmail.com>
2025-12-27 02:21:05 -0600

    Updated views with new webhook script

--- webhook/views.py
+++ webhook/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
+import json
+from django.http import JsonResponse, HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+
 
 # Create your views here.
 
 def webhook(request):
+    if request.method == 'POST':
+        payload = json.loads(request.body)
+        return JsonResponse({'status': 'success', 'message': 'Payload received'})
     return render(request, 'webhook.html')
